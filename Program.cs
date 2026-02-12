74a347e224005f9956a49498bbaf3d09b1fafebb
basheeralgunaid8-cmyk <basheeralgunaid8@gmail.com>
2026-02-12 14:08:25 +0800

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,23 +1,32 @@
-﻿using System;
-
-namespace Theatre_Square
+﻿namespace Next_Round
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] parts = Console.ReadLine().Split(' ');
+            string[] parts = Console.ReadLine().Split(" ");
+            int n = int.Parse(parts[0]);
+            int k= int.Parse(parts[1]);
+            int counter = 0;
+            string[] contestent  = Console.ReadLine().Split(" ");
+            int[] numbers = new int[n];
+            
 
-            long n = long.Parse(parts[0]);
-            long m = long.Parse(parts[1]);
-            long a = long.Parse(parts[2]);
+            for (int i=0;i<n;i++)
+            {
+                numbers[i] = int.Parse(contestent[i]);
+                int threshold = numbers[k - 1];
+                if (numbers[i] >= threshold && numbers[i] > 0)
+                {
+                    counter++;
+                }
+                else
+                    break;
 
-            long tilesAlongN = (long)Math.Ceiling((double)n / a);
-            long tilesAlongM = (long)Math.Ceiling((double)m / a);
+            }
 
-            long howManyTiles = tilesAlongN * tilesAlongM;
-
-            Console.WriteLine(howManyTiles);
+            Console.WriteLine(counter);
+           
         }
     }
 }
